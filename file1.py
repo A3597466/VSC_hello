32c4b77dd55ce1307372c9040e74af9358499147
wosunny2008 <wosunny@126.com>
2023-09-26 11:11:55 +0800

    v0.02

--- file1.py
+++ file1.py
@@ -4,6 +4,7 @@ import numpy as np
 msg0 = "hello hello "
 msg1 = "Hello world day! baby"
 msg2 = "bye bye"
-print(msg0+msg1+msg2)
+msg4 = "from git hub"
+print(msg0+msg1+msg2+msg4)
 
 print(np.random.randint(1, 9))
