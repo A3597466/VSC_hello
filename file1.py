779bcd39530428ddcece296ac49300c71d7c6802
wosunny2008 <wosunny@126.com>
2023-09-26 10:13:23 +0800

    Ver 0.01

---
+++ file1.py
@@ -0,0 +1,9 @@
+# this is py files
+import numpy as np
+
+msg0 = "hello hello "
+msg1 = "Hello world day! baby"
+msg2 = "bye bye"
+print(msg0+msg1+msg2)
+
+print(np.random.randint(1, 9))
---
+++ 编辑操场-VSC.js
@@ -0,0 +1,103 @@
+<div>
+    <header>
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </header>
+    <footer>
+        <p></p>
+    </footer>
+</div>
+
+
+const cars = ["🚗", "🚙", "🚕"];
+
+for (const car of cars) {
+    // Drive the car
+    console.log(`This is the car ${car}`);
+}
+
+// F2 批量名门变量
+
+// Reference the function
+new BookNew("War of the Worlds", "H G Wells");
+new BookNew("The Martian", "Andy Weir");
+
+/**
+ * Represents a book.
+ *
+ * @param {string} title Title of the book
+ * @param {string} author Who wrote the book
+ */
+function BookNew(title, author) {
+    this.title = title;
+    this.author = author;
+}
+
+
+{
+    "name": "John",
+        "age": 31,
+            "city": "New York"
+}
+
+
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
+context.strokeStyle = 'blue';
+context.lineWidth.toString;
+
+BookNew.name
+
+// Multi-curso and selection
+// Ctrl+L
+
+#p1 { background - color: #ff0000; }                /* red in HEX format */
+#p2 { background - color: hsl(120, 100 %, 50 %); }    /* green in HSL format */
+#p3 { background - color: rgba(0, 4, 255, 0.733); } /* blue with alpha channel in RGBA format */
+
+
+try {
+
+} catch (错误信息) {
+
+}
+
+
+
+function myname(num1) {
+    
+}
+
+do {
+    
+} while (n=100);
+
+new Promise((resolve, reject) => {
+    自动片段
+})
+
+<p>
+lorem40
+</p>
+
+
+console.log(log);
+
+define([
+    'require',
+    'dependency'
+], function(require, factory) {
+    'use strict';
+    
+});
+array.forEach(element => {
+    
+});
+
+
+array.forEach(element => {
+    
+});
